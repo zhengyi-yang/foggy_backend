76db4301ae362b007662a9e58801bc083dcfa1eb
Zhengyi Yang <zhengyi-yang@users.noreply.github.com>
2015-11-23 12:40:31 +0000

    Update DataInterp.rb
    Some bugs fixed

--- lib/assets/DataInterp.rb
+++ lib/assets/DataInterp.rb
@@ -1,9 +1,11 @@
 #Author: Zhengyi Yang
-#Date: 20 Nov, 2015
+#Created on Fri 20 Nov, 2015
 
 require 'rubypython'
+require 'pathname'
 
 class DataInterp
+
 #This class performs a 2 dimensional cubic spline interpolation on a given dataset.
 #The interpolation is supported by Python(version 2.x) SciPy library and a Ruby-Python 
 #bridge is built using rubypython.
@@ -13,19 +15,18 @@ class DataInterp
 # $: gem install rubypython 
 
 	def initialize(longs,lats,aqis)
-	#This method creates a new DataInterp instance.
 	#longs -- an array of longitudes of all data points
 	#lats -- an array of latitudes of all data points
 	#aqis -- an array of AQIs(pollution levels) of all data points
 	#Note that at least 16 data points is needed to estimate the function.
-	
+
 		raise ArgumentError, "Arguments must have the same size" unless longs.size==lats.size and lats.size==aqis.size
 		raise ArgumentError, "At least 16 data points is required" unless longs.size>=16
 
-		RubyPython.start
-		interp=RubyPython.import("2d_interpolation")
+		startPy
+		interp=RubyPython.import('DataInterp')
 		interp.gen_func(longs,lats,aqis)
-		RubyPython.stop
+		stopPy
 	end
 
 	def calc(longs,lats)
@@ -34,13 +35,35 @@ class DataInterp
 	#lats -- an array of latitudes of given locations.
 	#return -- an array of AQIs of given locations.
 
-		RubyPython.start
-		interp=RubyPython.import("2d_interpolation")
+		startPy
+		interp=RubyPython.import('DataInterp')
 		aqis=interp.load_func(longs,lats).rubify
-		RubyPython.stop
+		stopPy
 		return aqis
 	end
+
+
+	private
+	def get_current_path
+		return Pathname.new(File.dirname(__FILE__)).realpath.to_s
+	end
+
+	private
+	def startPy
+		RubyPython.start
+		sys=RubyPython.import('sys')
+		sys.path.append(get_current_path)
+		os=RubyPython.import('os')
+		os.chdir(get_current_path)
+	end
 	
+	private
+	def stopPy
+		RubyPython.stop
+	end
+
 end
+
+
 
 
