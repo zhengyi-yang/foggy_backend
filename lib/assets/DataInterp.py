386e412789057eae29c1ff225245cd2d3472d993
Zhengyi Yang <zhengyi-yang@users.noreply.github.com>
2015-11-23 12:42:06 +0000

    Update and rename 2d_interpolation.py to DataInterp.py
    Some bugs fixed

--- lib/assets/DataInterp.py
+++ lib/assets/DataInterp.py
@@ -1,14 +1,14 @@
 #Author: Zhengyi Yang
-#Date: 20 Nov, 2015
+#Created on Fri 20 Nov, 2015
 
-from numpy import array
+from numpy import array,float64
 from scipy.interpolate import interp2d
 import pickle
 
 def gen_func(x,y,z):
-	x=array(x,dtype=np.float64)
-	y=array(y,dtype=np.float64)
-	z=array(z,dtype=np.float64)
+	x=array(x,dtype=float64)
+	y=array(y,dtype=float64)
+	z=array(z,dtype=float64)
 	f = interp2d(x, y, z, kind='cubic')
 	with open('estimated_func.pkl', 'wb') as output:
     		pickle.dump(f, output, pickle.HIGHEST_PROTOCOL)
@@ -16,8 +16,11 @@ def gen_func(x,y,z):
 def load_func(x,y):
 	with open('estimated_func.pkl', 'rb') as input:
     		f = pickle.load(input)
-	x=array(x,dtype=np.float64)
-	y=array(y,dtype=np.float64)
+	x=array(x,dtype=float64)
+	y=array(y,dtype=float64)
 	return list(f(x,y))
+
+
+
 
 
