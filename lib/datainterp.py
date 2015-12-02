2534ab02aaa1139587febc4e0cde5fd17f290cd6
Zhengyi Yang <zhengyi-yang@users.noreply.github.com>
2015-12-02 20:36:28 +0000

    ISSUE 20 - Interpolation of pollution data

--- lib/datainterp.py
+++ lib/datainterp.py
@@ -8,7 +8,7 @@ import pickle
 def gen_func(x,y,z):
         points=array(zip(x,y),dtype=float64)
         z=array(z,dtype=float64)
-	f = CloughTocher2DInterpolator(points,z)
+	f = CloughTocher2DInterpolator(points,z)#Clough-Tocher method
 	with open('estimated_func.pkl', 'wb') as output:
     		pickle.dump(f, output, pickle.HIGHEST_PROTOCOL)
 
@@ -23,3 +23,37 @@ def load_func(x,y,isCoord=False):
 	else:
                 ans=f(x,y)
 	return ans.tolist()
+
+'''
+def make_graph(lon=None,lat=None,aqi=None):
+        from mpl_toolkits.mplot3d import axes3d
+        from numpy import random,linspace
+        from matplotlib import pyplot as plt
+
+        if(x==None or y==None or z==None):
+            lon=50+random.rand(20)
+            lat=random.rand(20)
+            aqi=random.randint(1,3,20)
+        gen_func(lon,lat,aqi)
+
+        min_lon,max_lon=min(lon),max(lon)
+        min_lat,max_lat=min(lat),max(lat)
+
+        x=linspace(min_lon,max_lon,100)
+        y=linspace(min_lat,max_lat,100)
+        xx,yy=meshgrid(x,y)
+
+        z=load_func(xx,yy)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        #ax.set_zlim(0,9)
+        ax.plot(lon,lat,aqi,'ro')
+        ax.plot_surface(xx,yy,z,rstride=2,cstride=2,cmap=plt.cm.coolwarm,alpha=0.8)
+        ax.set_xlabel('longitude')
+        ax.set_ylabel('latitude')
+        ax.set_zlabel('aqi')
+
+        plt.show()
+'''
+
