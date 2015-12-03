0bac38777fd22d4ee44680be02ebe899b7353c56
Zhengyi Yang <zhengyi-yang@users.noreply.github.com>
2015-12-03 11:47:56 +0000

    ISSUE 20 - Interpolation of pollution data - fixup

--- lib/datainterp.py
+++ lib/datainterp.py
@@ -6,32 +6,41 @@ from scipy.interpolate import CloughTocher2DInterpolator
 import pickle
 
 def gen_func(x,y,z):
-    points=array(zip(x,y),dtype=float64)
-    z=array(z,dtype=float64)
-    f = CloughTocher2DInterpolator(points,z)#Clough-Tocher method
-    with open('estimated_func.pkl', 'wb') as output:
-        pickle.dump(f, output, pickle.HIGHEST_PROTOCOL)
+    ziped_xyz=zip(zip(x,y),z)
+    ziped_xyz=[x for x in ziped_xyz if x[1]!=0]
+    unziped_xyz=zip(*ziped_xyz)
+    points=array(unziped_xyz[0],dtype=float64)
+    z=array(unziped_xyz[1],dtype=float64)
+    
+    #file=open('datainterp_debug.txt','w')
+    #file.write(str(points)+"\n\n"+str(z)+"\n\n")
+        
+	f = CloughTocher2DInterpolator(points,z)#Clough-Tocher method
+	with open('estimated_func.pkl', 'wb') as output:
+    		pickle.dump(f, output, pickle.HIGHEST_PROTOCOL)
+    #file.write(str(f(array(x),array(y))))	
+    #file.close()
 
 def load_func(x,y,isCoord=False):
-    with open('estimated_func.pkl', 'rb') as input:
-        f = pickle.load(input)
-    x=array(x,dtype=float64)
-    y=array(y,dtype=float64)
-    if isCoord:
-        xx,yy=meshgrid(x,y)
-        ans=f(xx,yy)
-    else:
-        ans=f(x,y)
-    ans=ans.tolist()
-    return ans
+	with open('estimated_func.pkl', 'rb') as input:
+    		f = pickle.load(input)
+	x=array(x,dtype=float64)
+	y=array(y,dtype=float64)
+	if isCoord:
+		xx,yy=meshgrid(x,y)
+                ans=f(xx,yy).tolist
+	else:
+                ans=f(x,y)
+	ans=ans.tolist()
+	return ans
 
 '''
 def make_graph(lon=None,lat=None,aqi=None):
         from mpl_toolkits.mplot3d import axes3d
-        from numpy import random,linspace
+        from numpy import random,linspace,array
         from matplotlib import pyplot as plt
 
-        if(x==None or y==None or z==None):
+        if(lon==None or lat==None or aqi==None):
             lon=50+random.rand(20)
             lat=random.rand(20)
             aqi=random.randint(1,3,20)
@@ -56,5 +65,32 @@ def make_graph(lon=None,lat=None,aqi=None):
         ax.set_zlabel('aqi')
 
         plt.show()
+
+
+a=array([[ 51.563752  ,   0.177891  ,   1.        ],
+       [ 51.529389  ,   0.132857  ,   1.        ],
+       [ 51.49464868,   0.13727911,   2.        ],
+       [ 51.46598327,   0.18487713,   2.        ],
+       [ 51.49061021,   0.15891449,   1.        ],
+       [ 51.4563    ,   0.085606  ,   2.        ],
+       [ 51.552476  ,  -0.258089  ,   2.        ],
+       [ 51.537799  ,  -0.247793  ,   2.        ],
+       [ 51.522287  ,  -0.125848  ,   2.        ],
+       [ 51.544219  ,  -0.175284  ,   2.        ]])
+
+x=[];y=[];z=[]
+
+for i in a:
+  x.append(i[0])
+  y.append(i[1])
+  z.append(i[2])
+
+gen_func(x,y,z)
+print load_func(x,y)
+print z
+print load_func(0.5,0.5)
+
+make_graph(x,y,z)
 '''
+
 
